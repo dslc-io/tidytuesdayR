7e61a786bb7cf20bde3ecb020e1daebd50b4df7b
Ellis Hughes <ehhughes@scharp.org>
2019-02-12 11:47:40 -0800

    fixed css for github

--- R/utils.R
+++ R/utils.R
@@ -27,8 +27,7 @@ tt_make_html<-function(x){
   tmpHTML<-tempfile(fileext = ".html")
   cat("<!DOCTYPE html><html lang=\"en\"><head>
       <link rel=\"dns-prefetch\" href=\"https://github.githubassets.com\">
-      <link crossorigin=\"anonymous\" media=\"all\" rel=\"stylesheet\" href=\"https://github.githubassets.com/assets/frameworks-a3b8a10d4a9e37a78f033ef4a4f525f5.css\">
-      <link crossorigin=\"anonymous\" media=\"all\" rel=\"stylesheet\" href=\"https://github.githubassets.com/assets/github-9bcb96f570b0873a1a19187b85566f30.css\">
+      <link crossorigin=\"anonymous\" media=\"all\" rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css\">
       </head><body>",file=tmpHTML)
   cat(x[['tt']]$readme,file = tmpHTML,append = TRUE)
   cat("</body></html>",file = tmpHTML,append = TRUE)
