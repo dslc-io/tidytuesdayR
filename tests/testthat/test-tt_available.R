cae0db82fff63aedf691a10cda00a3e7a19680b2
Jon Harmon <jonthegeek@gmail.com>
2019-04-06 12:45:41 -0500

    Ran styler::style_pkg and did some manual clean up to make things easier to read.

--- tests/testthat/test-tt_available.R
+++ tests/testthat/test-tt_available.R
@@ -1,19 +1,19 @@
 context("test-tt_available")
 
 test_that("tt_available returns object of class 'tt_dataset_table_list", {
-  ds<-tt_available()
-  testthat::expect_s3_class(ds,"tt_dataset_table_list")
-  })
+  ds <- tt_available()
+  testthat::expect_s3_class(ds, "tt_dataset_table_list")
+})
 
 test_that("tt_available returns all years", {
-  ds<-tt_available()
-  years<-tt_years()
-  testthat::expect_equivalent(names(ds),years[order(years,decreasing = TRUE)])
+  ds <- tt_available()
+  years <- tt_years()
+  testthat::expect_equivalent(names(ds), years[order(years, decreasing = TRUE)])
 })
 
 
 test_that("tt_datasets prints to console when rstudio viewer is not available", {
-  ds<-tt_datasets(2018)
-  consoleOutput<-print(ds,printConsole=TRUE)
-  testthat::expect_equivalent(ds$html%>%rvest::html_table(),consoleOutput)
+  ds <- tt_datasets(2018)
+  consoleOutput <- print(ds, printConsole = TRUE)
+  testthat::expect_equivalent(ds$html %>% rvest::html_table(), consoleOutput)
 })
