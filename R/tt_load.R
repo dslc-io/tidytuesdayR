9f9031a3f7bb2b73003b0a5124e6cfedc5ea377a
Ellis Hughes <ehhughes@scharp.org>
2019-11-06 16:07:41 -0800

    improve read speed, and add user ability to pass args to tt_read_data from tt_load

--- R/tt_load.R
+++ R/tt_load.R
@@ -2,6 +2,7 @@
 #'
 #' @param x string representation of the date of data to pull, in YYYY-MM-dd format, or just numeric entry for year
 #' @param week left empty unless x is a numeric year entry, in which case the week of interest should be entered
+#' @param ... pass methods to the parsing functions. These will be passed to ALL files, so be careful.
 #' @return tt_data object (list class)
 #'
 #' @export
@@ -10,9 +11,9 @@
 #'
 #' @examples
 #' tt_output <- tt_load("2019-01-15")
-tt_load <- function(x, week) {
+tt_load <- function(x, week, ...) {
   tt <- tt_load_gh(x, week)
-  tt_data <- purrr::map(attr(tt, ".files"), ~ tt_read_data(tt, .x))
+  tt_data <- purrr::map(attr(tt, ".files"), function(x) tt_read_data(tt, x, ... ))
   names(tt_data) <- tools::file_path_sans_ext(attr(tt, ".files"))
 
   structure(
@@ -22,13 +23,13 @@ tt_load <- function(x, week) {
   )
 }
 
-#' @title access data in tt_data object
-#' @param x tt_data object
-#' @param name name of dataset to access
-#' @exportMethod
-`$.tt_data` <-function(x,name){
-  x[[name]]
-}
+# #' @title access data in tt_data object
+# #' @param x tt_data object
+# #' @param name name of dataset to access
+# #' @exportMethod `$`
+# `$.tt_data` <-function(x,name){
+#   x[[ name ]]
+# }
 
 #' @title  Load TidyTuesday data from Github
 #'
--- R/tt_read_data.R
+++ R/tt_read_data.R
@@ -4,6 +4,7 @@
 #'
 #' @param tt tt_gh object from tt_load_gh function
 #' @param x index/name of data object to read in. string or int
+#' @param guess_max number of rows to use to esimate col type, defaults to 5000. Only used for text files.
 #' @return tibble
 #' @export
 #'
@@ -17,21 +18,21 @@
 #' tt_gh <- tt_load_gh("2019-01-15")
 #'
 #' tt_dataset_1 <- tt_read_data(tt_gh, tt_gh[1])
-tt_read_data <- function(tt, x) {
+tt_read_data <- function(tt, x, guess_max = 5000) {
   suppressMessages({
     switch(class(x),
-      "character" = tt_read_data.character(tt, x),
-      "numeric" = tt_read_data.numeric(tt, x),
-      "integer" = tt_read_data.numeric(tt, x),
+      "character" = tt_read_data.character(tt, x, guess_max = guess_max ),
+      "numeric" = tt_read_data.numeric(tt, x, guess_max = guess_max ),
+      "integer" = tt_read_data.numeric(tt, x, guess_max = guess_max ),
       stop(paste("No method for entry of class:", class(x)))
     )
   })
 }
 
-tt_read_data.character <- function(tt, x) {
+tt_read_data.character <- function(tt, x, guess_max = 5000) {
   if (x %in% attr(tt, ".files")) {
     url <- paste0(gsub("tree", "blob", file.path(attr(tt, ".url"), x)), "?raw=true")
-    tt_read_url(url)
+    tt_read_url(url, guess_max = guess_max)
   } else {
     stop(paste0(
       "That is not an available file for this TidyTuesday week!\nAvailable Datasets:\n",
@@ -40,10 +41,10 @@ tt_read_data.character <- function(tt, x) {
   }
 }
 
-tt_read_data.numeric <- function(tt, x) {
+tt_read_data.numeric <- function(tt, x, guess_max = 5000) {
   if (x > 0 & x <= length(attr(tt, ".files"))) {
     url <- paste0(gsub("tree", "blob", file.path(attr(tt, ".url"), attr(tt, ".files")[x])), "?raw=true")
-    tt_read_url(url)
+    tt_read_url(url, guess_max = guess_max)
   } else {
     stop(paste0(
       "That is not an available index for the files for this TidyTuesday week!\nAvailable Datasets:\n\t",
@@ -53,14 +54,14 @@ tt_read_data.numeric <- function(tt, x) {
 }
 
 
-tt_read_url <- function(url) {
+tt_read_url <- function(url, guess_max = 5000) {
   url <- gsub(" ", "%20", url)
   switch(tools::file_ext(gsub("[?]raw=true", "", tolower(url))),
-    "xls"  = download_read(url, readxl::read_xls, mode = "wb"),
-    "xlsx" = download_read(url, readxl::read_xlsx, mode = "wb"),
+    "xls"  = download_read(url, readxl::read_xls, guess_max = guess_max, mode = "wb"),
+    "xlsx" = download_read(url, readxl::read_xlsx, guess_max = guess_max, mode = "wb"),
     "rds"  = download_read(url, readRDS, mode = "wb"),
     "rda"  = download_read(url, read_rda, mode = "wb"),
-    download_read(url, readr::read_delim, guess_max = 21474836, progress = FALSE, find_delim = TRUE)
+    download_read(url, readr::read_delim, guess_max = guess_max, progress = FALSE, find_delim = TRUE)
   )
 }
 
@@ -73,7 +74,7 @@ tt_read_url <- function(url) {
 #' @param find_delim should the delimeters be found for the file
 #' @importFrom utils download.file
 #'
-download_read <- function(url, func, ..., mode = "w", find_delim = FALSE) {
+download_read <- function(url, func, ..., guess_max, mode = "w", find_delim = FALSE) {
   temp_file <- tempfile(fileext = paste0(".", tools::file_ext(url)))
   utils::download.file(url, temp_file, quiet = TRUE, mode = mode)
 
@@ -88,17 +89,19 @@ download_read <- function(url, func, ..., mode = "w", find_delim = FALSE) {
     }
   }
 
+  if("guess_max"%in%names(as.list(args(func)))){
+    func_call$guess_max = guess_max
+  }
+
   return(eval(as.call(func_call)))
 }
 
 #' @title utility to load RDA with out using assigned name in envir
 #'
 #' @param path path to RDA file
-#' @importFrom base load
-#' @importFrom base new.env
 #
 read_rda <- function(path){
   load_env<-new.env()
   load(path,envir = load_env)
-  load_env[[ls(load_env)[1]]]
+  load_env[[ ls(load_env)[1] ]]
 }
