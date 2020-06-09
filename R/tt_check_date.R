0134265bbfe07db3037f6a39f2bdecd68be38699
Ellis Hughes <ehhughes@scharp.org>
2020-06-09 16:51:52 -0700

    remove rd for tt_check_date

--- R/tt_check_date.R
+++ R/tt_check_date.R
@@ -2,7 +2,7 @@
 #' @description Given multiple types of inputs, generate
 #' @param x either a string or numeric entry indicating the full date of
 #' @param week left empty unless x is a numeric year entry, in which case the week of interest should be entered
-#'
+#' @noRd
 tt_check_date <- function(x, week) {
   if (valid_date(x)) {
     tt_check_date.date(x)
