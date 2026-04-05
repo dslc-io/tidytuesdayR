9cee39984b0eadad24834ed93d88bb78d50fdd55
Jon Harmon <jonthegeek@gmail.com>
2026-04-05 17:12:41 -0500

    fix: don't double up on shared-params

--- R/aaa-shared_params.R
+++ R/aaa-shared_params.R
@@ -1,10 +1,21 @@
-#' Shared parameters
+#' Parameters used in multiple functions
 #'
-#' These parameters are used in multiple functions. They are defined here to
-#' make them easier to import and to find.
+#' Reused parameter definitions are gathered here for easier editing.
 #'
+#' @param auth A GitHub token. See [gh::gh_token()] for more details.
 #' @param call (`environment`) The caller environment for error messages.
+#' @param files Which file names to download. Default "All" downloads all files
+#'   for the specified week.
+#' @param path The relative path to the directory to hold your submission files
+#'   (`tt_submission` by default). If this directory does not exist, it will be
+#'   created.
+#' @param tt A `tt` object, output from [tt_load_gh()].
+#' @param week Which week number to use within a given year. Only used when `x`
+#'   is a valid year.
+#' @param x The date of data to pull (in "YYYY-MM-dd" format), or the four-digit
+#'   year as a number.
+#' @param year What year of TidyTuesday to use
 #'
-#' @name .shared-params
+#' @name shared-params
 #' @keywords internal
 NULL
